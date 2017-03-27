ae151d6dac6268a627f9fc86fc1c5126a5af90e0
benjaminantell <ba2364@tc.columbia.edu>
2017-03-27 19:54:15 -0400

    commit

--- app/src/main/java/com/ba2364/march27/MainActivity.java
+++ app/src/main/java/com/ba2364/march27/MainActivity.java
@@ -1,13 +1,40 @@
 package com.ba2364.march27;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView displayText;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference personRef = database.getReference("person");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = (TextView) findViewById(R.id.display_text);
+
+        personRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Person user = dataSnapshot.getValue(Person.class);
+                displayText.setText(user.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error loading Firebase", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ba2364/march27/Person.java
@@ -0,0 +1,33 @@
+package com.ba2364.march27;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean alive;
+
+    public Person() {
+    } // Empty constructor is needed to read from Firebase
+
+    public Person(String name, int age, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.alive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + " " + age + " " + alive;
+    }
+}
